12870dde0b792e48675caf42c574dc97da4489e4
XiaNengQi <920986617@qq.com>
2021-07-17 19:00:48 +0800

    Initial Commit

---
+++ XExpenso-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XExpenso-iOS
+//
+//  Created by 夏能啟 on 7/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XExpenso-iOS/XExpenso_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  XExpenso_iOSApp.swift
+//  XExpenso-iOS
+//
+//  Created by 夏能啟 on 7/17/21.
+//
+
+import SwiftUI
+
+@main
+struct XExpenso_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
